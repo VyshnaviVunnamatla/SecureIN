d86043c55679a434cfb0941a8ab9c90102e888af
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-27 15:29:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log("Backend running on 5000")))
+  .then(() => app.listen(PORT, () => console.log(`Backend running on ${PORT}`)))
   .catch(err => console.error("DB Connection Failed", err));
