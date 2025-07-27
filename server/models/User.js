946eaf666cc28d6fc636aa995fe178d1dcdf204d
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-27 11:42:47 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
   deviceHistory: [String],
   ipHistory: [String],
   suspiciousFlag: { type: Boolean, default: false },
