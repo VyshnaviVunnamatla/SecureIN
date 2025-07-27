6d49132f6955a7a3bc490764effe6f28a36e52d2
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-27 15:22:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://zero-trust-auth-system.vercel.app",  
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
