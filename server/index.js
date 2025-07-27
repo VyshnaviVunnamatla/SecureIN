d2ceaacb33d444a4506616e8ff47cf29de60b43d
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-27 10:46:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/auth", authRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log("Server running on 5000")))
-  .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Backend running on 5000")))
+  .catch(err => console.error("DB Connection Failed", err));
