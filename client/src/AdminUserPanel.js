519b2c9730c6a3e5857d2a8c88dfcec75892ecb8
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-27 18:10:37 +0530

    Update AdminUserPanel.js

--- client/src/AdminUserPanel.js
+++ client/src/AdminUserPanel.js
@@ -6,7 +6,7 @@ const AdminUserPanel = () => {
   const [reload, setReload] = useState(false);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/auth/admin/users `)
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/auth/admin/users`)
       .then(res => setUsers(res.data))
       .catch(err => console.error(err));
   }, [reload]);
