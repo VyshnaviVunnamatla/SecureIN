5e9fcbe68c3398a2fd86d7b2060ba5ba5ffe7ae9
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-27 10:36:40 +0530

    Create index.js

---
+++ backend/server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Server running on 5000")))
+  .catch((err) => console.error(err));
